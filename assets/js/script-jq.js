767d15f75fd06402554ff83f504fc1fa5dcc750b
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2023-08-06 19:35:51 +0300

    Delete assets directory

--- assets/js/script-jq.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
-        $('.header__burger').toggleClass('burger-active');
-        $('.header__menu-list').toggleClass('mobile-menu-active');
-        $('body').toggleClass('body-lock');
-    });
-});
